5abaaeb0fd2ed8613b79b9ae31bdf1ee51ca45f8
Сергей Кислицин <sergey.kislitsin.86@yandex.ru>
2024-11-15 15:37:30 +0700

    Добавлена возможность узнавания пользователя, если он указал свое имя в сообщении

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import telebot
+from PIL.ImageStat import Global
 from lxml.html.diff import token
 
 token = '7581581367:AAEfvQRx9i8_L-D-13nJSpjhxWkuaE5-jEs'
 
 bot = telebot.TeleBot(token)
 
+Name = 'Вася'
+
 @bot.message_handler(content_types=["text"])
 def echo(message):
-    bot.send_message(message.chat.id, message.text)
+    if Name in message.text:
+        text = 'Ба! Знакомые все лица'
+    else:
+        text = message.text
+    bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
